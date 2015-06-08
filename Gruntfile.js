795f9c9f9a78e95e9c6a14d72fcb3a8aa2e05d38
Stephanie Aviles <avilestephcode@gmail.com>
2015-06-08 14:43:53 -0400

    added config and loading tasks inside gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+	// project configuration 
+	grunt.initConfig({});
+
+	// loading tasks (plugins)
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// register tasks
+	// grunt.registerTask('default', ['uglify']);
+
+};
