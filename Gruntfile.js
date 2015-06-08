40f0d87d874826c728a4d5d6a468d46e13e41035
joeybones <joe.hart.reads.this@gmail.com>
2015-06-08 16:32:25 -0400

    jshinting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,23 @@
 module.exports = function(grunt) {
+	'use strict';
 
-	// project configuration 
-	grunt.initConfig({});
+	// project configuration
+	grunt.initConfig({
+		jshint: {
+            options: {
+            	jshintrc: {
+					"curly": true,
+					"eqnull": true,
+					"eqeqeq": true,
+					"undef": true,
+					"globals": {
+				    	"jQuery": true
+				 	}
+				},
+				all: ['/js/*.js']
+            }
+        }
+	});
 
 	// loading tasks (plugins)
 	grunt.loadNpmTasks('grunt-contrib-jshint');
